c5e676cef03aa50db27f3ef8c060065c291cad25
Aleksandra-Shevchenko <shevdorosh@yandex.ru>
2021-07-08 19:31:50 +0300

    responsive-design

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,35 +1,39 @@
 import './index.css';
-
-import '../slider/chief-slider.js';
 import { ChiefSlider } from "../slider/chief-slider.js";
+import FormValidator from "../components/FormValidator.js";
 
 
-// document.addEventListener('DOMContentLoaded', function () {
-//   new ChiefSlider('.slider', {
-//     loop: true,
-//     autoplay: true,
-//     interval: 5000,
-//     refresh: true,
-//   });
-// });
+// создаем экземпляры слайдеров
+document.addEventListener('DOMContentLoaded', function() {
+  var slidersEl = document.querySelectorAll('.slider');
+  for (var i = 0, len = slidersEl.length; i < len; i++) {
+    // инициализация каждого элемента в качестве слайдера
+    new ChiefSlider(slidersEl[i], {
+      loop: false,
+      autoplay: true,
+      interval: 5000,
+      refresh: true,
+    });
+  };
+});
 
 
-const slidersEl = document.querySelectorAll('.slider');
-for (let i = 0, len = slidersEl.length; i < len; i++) {
-  // инициализация каждого элемента в качестве слайдера
-  new ChiefSlider(slidersEl[i], {
-    loop: false,
-    // autoplay: true,
-    interval: 5000,
-    refresh: true,
-  });
+//объект параметров для валидации форм
+const validationObject = {
+  formSelector: '.footer__form',
+  inputSelector: '.footer__input',
+  submitButtonSelector: '.footer__btn',
+  inactiveButtonClass: 'footer__btn_inactive',
+  inputErrorClass: 'footer__input-error',
+  errorClass: 'footer__input-error_active',
 };
 
-// const slider = new ChiefSlider('.slider', {
-//   loop: false,
-//   // autoplay: true,
-//   interval: 5000,
-//   refresh: true,
-// });
+//селекторы для создания экземпляров классов
+const selectorObj = {
+  form: '.footer',
+};
 
-// console.log(slider);
+
+//создаем экземпляры класса FormValidator и включаем валидацию форм
+const validForm = new FormValidator(validationObject, selectorObj.form);
+validForm.enableValidation();
